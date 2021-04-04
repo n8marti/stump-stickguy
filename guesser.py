2ec5c20bca8812d627fbb342023ab55f0baa4e2e
Nate Marti <nate_marti@sil.org>
2021-04-04 18:21:01 +0100

    added functional components

---
+++ guesser.py
@@ -0,0 +1,23 @@
+import math
+
+from random import randint
+
+
+def set_guess_range(lowest, highest):
+    quarter1 = int((highest + 1 - lowest) / 4)
+    quarter3 = 3 * quarter1
+    guess_min = lowest + quarter1
+    guess_max = lowest + quarter3
+    return guess_min, guess_max
+
+def get_confidence(rem_possibilities, rem_guesses):
+    confidence = round(1 / rem_possibilities ** (1 / rem_guesses), 2) * 100
+    return confidence
+
+def get_guess(guess_min, guess_max):
+    guess = randint(guess_min, guess_max)
+    return guess
+
+def get_max_guesses(max_number):
+    max_guesses = int(round(math.log2(max_number)) + round(math.log(max_number, 1000)))
+    return max_guesses
---
+++ plotter.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+
+def draw_numberline(ymin, ymax, previous_guesses, current_limits, current_guess):
+    fontsize = 24
+
+    # Set up the figure.
+    fig, ax = plt.subplots(figsize=(3, 5))
+    ax.set_xlim(0, 2)
+    ax.set_ylim(0, ymax + 1)
+
+    # Draw lines.
+    x = 1
+    width = 3
+
+    plt.vlines(x, ymin, ymax)
+    plt.hlines(ymin, x - width / 10., x + width / 10.)
+    plt.hlines(ymax, x - width / 10., x + width / 10.)
+
+    # Draw previous guesses as individual ticks.
+    for guess in previous_guesses:
+        plt.plot(x, guess, marker='_', markersize=10, color='grey')
+
+    # Draw current limits as a line.
+    plt.plot([x, x], current_limits, marker='_', markersize=15, color='red', linewidth=5)
+
+    # Draw current guess as a single tick and label it.
+    plt.plot(
+        x,
+        current_guess,
+        marker='_',
+        markersize=20,
+        color='black',
+    )
+    plt.text(
+        x - width / 3,
+        current_guess - ymax / 50,
+        f"{current_guess}?",
+        fontsize=fontsize,
+    )
+
+    # Add ymin and ymax numbers.
+    plt.text(
+        x,
+        ymin - ymax / 10,
+        ymin,
+        horizontalalignment='center',
+        fontsize=fontsize
+    )
+    plt.text(
+        x,
+        ymax * 1.05,
+        ymax,
+        horizontalalignment='center',
+        fontsize=fontsize
+    )
+
+    plt.axis('off')
+    fig.savefig('data/last_numberline.png', transparent=True)
+    return fig
